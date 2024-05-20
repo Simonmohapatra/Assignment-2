56c0e31f40bb2674d12653087d6333f3f1ede872
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-20 20:24:19 +0530

    Q6.java

---
+++ Q6.java
@@ -0,0 +1,11 @@
+/*
+Create an abstract class Marks with three instance variables (markICP, markDSA,
+and percentage) and an abstract method getPercentage(). Create two classes: CSE
+with instance variable algoDesign, and NonCSE with instance variable enggMechanics. Both classes inherit the abstract class Marks and override the abstract method
+getPercentage(). The constructor of class CSE takes the marks in three subjects
+(markICP, markDSA, and algoDesign) as its parameters, and the constructor of class
+NonCSE takes the marks in three subjects (markICP, markDSA, and enggMechanics)
+as its parameters. Create an object for each of the two classes and print the percentage
+of marks for both students.
+*/
+
