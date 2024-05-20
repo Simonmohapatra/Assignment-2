8442208abee07ab7a6aaee31d58d11a4db65aee2
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-20 15:20:18 +0530

    Q4.java

--- Q4.java
+++ Q4.java
@@ -18,43 +18,43 @@ totalAamt = Principal + (PrincipalÖrateÖTime)/100;
 */
 
 class Deposit{
-    long principal;
-    int time;
-    double rate;
-    double totalAmt;
+	long principal;
+        int time;
+        double rate;
+        double totalAmt;
 
     Deposit(){
-        principal=100;
-		time=1;
-		rate=5.7;
+	    principal=100;
+	    time=1;
+	    rate=5.7;
     }
     Deposit(long principal, int time, double rate){
-        this.principal=principal;
-        this.time=time;
-        this.rate=rate;
+	    this.principal=principal;
+	    this.time=time;
+	    this.rate=rate;
     }
     Deposit(long principal, int time){
-        this.principal=principal;
-        this.time=time;
+	    this.principal=principal;
+	    this.time=time;
     }
     Deposit(long principal, double rate){
-        this.principal=principal;
-        this.rate=rate;
+	    this.principal=principal;
+            this.rate=rate;
     }
     void display(){
-        System.out.println("Principal: "+principal+);
-        System.out.println("Time: "+time);
-        System.out.println("Rate: "+rate);
+	    System.out.println("Principal: "+principal+);
+            System.out.println("Time: "+time);
+            System.out.println("Rate: "+rate);
     }
     double calcAmt(){
-        totalAmt= principal+(principal*rate*time)/100;
-        return totalAmt;
+	    totalAmt= principal+(principal*rate*time)/100;
+            return totalAmt;
     }
 }
 
 public class Q4{
-    public static void main(String[] args) {
-        Deposit d= new Deposit(1000,2,6);
-        System.out.println("The total amount is: "+ d.calcAmt());
-    }
+	public static void main(String[] args) {
+		Deposit d= new Deposit(1000,2,6);
+		System.out.println("The total amount is: "+ d.calcAmt());
+	}
 }
