0858da5500d581e08cfd8d817f6ac28a504922ec
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-27 22:31:12 +0530

    HQ4.java

---
+++ HQ4.java
@@ -0,0 +1,5 @@
+/*
+Write a Java code that would represent Distance object (meters, centimetres) using classes. 
+The class definition include a parameterized constructor, void display ( ) to display the instance variables, void sum(Distance, Distance) to add two distances. 
+Now write a main function that creates a couple of Distance objects and demonstrates the addition of two distances.
+*/
