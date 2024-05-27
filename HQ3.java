0646b3293122603daa597b2a3a7c4af5aa6793c0
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-27 22:29:50 +0530

    HQ3.java

---
+++ HQ3.java
@@ -0,0 +1,8 @@
+/*
+Create a class Bank with two instance variables: bankNname, depositAamount and a class variable totalAamount. 
+Defined three member methods: setBankName() to set the bank name, setAmount() to set the deposit amount, and showData() for displaying the bank name and deposit amount. 
+A person visited 5 different banks and deposite money.
+Create 5 array object of class Bank set the bank name and deposit amount (the minimum deposit amount is 1000) using member methods setBankName() and setAmount(). 
+Call the showData() method to display the information with totalAmount deposited by the person. 
+Also, design a method that displays the bank name where the person deposits the minimum amount.
+*/
